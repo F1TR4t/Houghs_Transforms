98d178bc8e21abb0fe78f632823e6c9d5edd46fb
Mustafa <fitrat.mustafa@gmail.com>
2021-05-04 03:46:16 -0400

    Edge detection and edge extraction

--- hough.py
+++ hough.py
@@ -1,9 +1,11 @@
 # Our Imports
-import matplotlib.pyplot as plt
+# Starter code (run this first)
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+import scipy.interpolate
 import scipy.signal
-
-from PIL import Image
+from PIL import Image, ImageFilter
 
 # Method Area [For creating methods]
 
@@ -43,27 +45,52 @@ def hough_transform():
     return None
 
 def load_image(filepath):
-    # Loads an image into a numpy array.
-    # Note: image will have 3 color channels [r, g, b].
+    """Loads an image into a numpy array.
+    Note: image will have 3 color channels [r, g, b]."""
     img = Image.open(filepath)
-    return (np.asarray(img).astype(float)/255)[:, :,:3]
+    return (np.asarray(img).astype(np.float64)/255)[:, :, :3]
+
+def load_image_gray(filepath):
+    """Loads an image into a numpy array.
+    Note: image will have 3 color channels [r, g, b]."""
+    img = Image.open(filepath)
+    img = img.convert("L")
+    img.save("out_images/grey_lines1.png")
+    img = img.filter(ImageFilter.FIND_EDGES)
+    img.save("out_images/edge_lines1.png")
+    return (np.asarray(img).astype(np.float64)/255)[:, :]
+
+# Go through our Inputted Image Matrix and extract all edges (white locations)
+def extract_edges(img):
+    out_x = []
+    out_y = []
+
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            if img[y, x] == 1:
+                out_x.append(x)
+                out_y.append(y)
+    return out_x, out_y
 
 # Global Area [Just the area with we'll run our Code]
 
-i_x = [[1, 0, -1],[1, 0, -1],[1, 0, 1]]
-i_y = [[1, 1, 1],[0, 0, 0],[-1, -1, -1]]
+# Grab our Test Images, and get out Edge Detected Image
+lines1 = Image.open("test_images/lines1.png")
+l1_edge = lines1.convert("L")
+l1_edge = l1_edge.filter(ImageFilter.FIND_EDGES)
+l1_ed_mat = np.asarray(l1_edge).astype(np.float64)/255
 
-# Grab our Test Images and convert into Numpy Arrays
-lines1 = load_image("test_images/lines1.png")
-
-# Take derivatives of the image to extract the Image Gradient
-lines1_i_x = scipy.signal.convolve2d(lines1[:][:][0], i_x, mode='same')
-#lines1_i_x = scp.convolve2d(lines1, i_y)
-
+# Locate all edges within the Image, Edges should be white
+edges_x, edges_y = extract_edges(l1_ed_mat)
 # Pass down necesssary arguments to hough_transform()
 # grab what it returns 
 
+
+
 # Draw Lines then add that layer ontop of the original image, save into out_images/
 fig = plt.figure(figsize=(3, 3), dpi=300)
-plt.imshow(lines1_i_x, cmap='gray')
-plt.savefig("out_images/lines1_i_x")
+plt.axis('off')
+plt.imshow(lines1, cmap='gray')
+plt.savefig("out_images/lines1", bbox_inches="tight")
+plt.plot(edges_x, edges_y, 'o', markersize=0.1, color='firebrick', fillstyle="none")
+plt.savefig("out_images/lines1_edge", bbox_inches="tight")
